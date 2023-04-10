4a02381d7f422a5f64bb68db899a282e28f9a4c1
Jarryd Deane <Jarryd.deane@gmail.com>
2023-04-10 15:42:53 +0200

    Upload files to S3
    Programmatically upload files to S3

--- S3/S3Playground/Program.cs
+++ S3/S3Playground/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+await using var mapInputStream = new FileStream("./Map.png", FileMode.Open, FileAccess.Read);
+
+var mapPutObjectRequest = new PutObjectRequest()
+{
+    BucketName = "jarrydawscource",
+    Key = "images/Map.png",
+    ContentType = "image/png",
+    InputStream = mapInputStream
+};
+
+await s3Client.PutObjectAsync(mapPutObjectRequest).ConfigureAwait(false);
+
+await using var moviesInputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var moviesPutObjectRequest = new PutObjectRequest()
+{
+    BucketName = "jarrydawscource",
+    Key = "files/movies.csv",
+    ContentType = "text/csv",
+    InputStream = moviesInputStream
+};
+
+await s3Client.PutObjectAsync(moviesPutObjectRequest).ConfigureAwait(false);
